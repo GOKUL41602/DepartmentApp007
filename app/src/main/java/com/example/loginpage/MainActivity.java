cd07122c215b37bb1c5a7ab3628fbafab6b486e3
Gokul41602 <gokul.18it@kct.ac.in>
2021-03-22 19:02:13 +0700

    Completed login and register activity for both faculty and student.

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.loginpage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,14 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView facultyRegisterText, studentRegisterText;
 
-    private String registerKey = "null";
+    private String facultyUserNameText, facultyPasswordText, studentUserNameText, studentPasswordText, registerKey = "null";
 
     private TextInputLayout facultyUserName, facultyPassword, studentUserName, studentPassword;
 
     private RelativeLayout facultyRelLayout, studentRelLayout;
+
+    private FirebaseDatabase database;
+
+    private DatabaseReference reference;
+
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        facultyLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initializeFacultyStrings();
+
+            }
+        });
+
         facultyRegisterText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,6 +97,158 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        facultyLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initializeFacultyStrings();
+                if (validateFacultyUserName()) {
+                    if (validateFacultyPassword()) {
+                        verifyFacultyLoginDetails();
+                    } else {
+                        validateFacultyPassword();
+                    }
+                } else {
+                    validateFacultyUserName();
+                }
+            }
+        });
+
+        studentLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initializeStudentStrings();
+                if (validateStudentUserName()) {
+                    if (validateStudentPassword()) {
+                        verifyStudentLoginDetails();
+                    } else {
+                        validateStudentPassword();
+                    }
+                } else {
+                    validateStudentUserName();
+                }
+            }
+        });
+
+    }
+
+    private void verifyStudentLoginDetails() {
+        reference = database.getInstance().getReference("StudentsDto");
+        Query query = reference.orderByChild("studentRollNo").startAt(studentUserNameText).endAt(studentUserNameText + "\uf8ff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    studentUserName.setError(null);
+                    studentUserName.setErrorEnabled(false);
+                    String passwordFromDB = snapshot.child(studentUserNameText).child("studentPassword").getValue(String.class);
+                    if (studentPasswordText.equals(passwordFromDB)) {
+                        studentPassword.setError(null);
+                        studentPassword.setErrorEnabled(false);
+                        Snackbar.make(relativeLayout, "Student Login Successful", Snackbar.LENGTH_LONG).show();
+                    } else {
+                        studentPassword.setError("Incorrect Password");
+                        studentPassword.requestFocus();
+                    }
+                } else {
+                    studentUserName.setError("UerName doesn't exists");
+                    studentUserName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void verifyFacultyLoginDetails() {
+        reference = database.getInstance().getReference("Faculty'sDto");
+        Query query = reference.orderByChild("facultyRollNo").startAt(facultyUserNameText).endAt(facultyUserNameText + "\uf8ff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    facultyUserName.setError(null);
+                    facultyUserName.setErrorEnabled(false);
+                    String passwordFromDB = snapshot.child(facultyUserNameText).child("facultyPassword").getValue(String.class);
+                    if (facultyPasswordText.equals(passwordFromDB)) {
+                        facultyPassword.setError(null);
+                        facultyPassword.setErrorEnabled(false);
+                        Snackbar.make(relativeLayout, "Faculty Login Successful", Snackbar.LENGTH_LONG).show();
+                    } else {
+                        facultyPassword.setError("Incorrect Password");
+                        facultyPassword.requestFocus();
+                    }
+                } else {
+                    facultyUserName.setError("UerName doesn't exists");
+                    facultyUserName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private boolean validateFacultyUserName() {
+        if (facultyUserNameText.length() != 8) {
+            facultyUserName.setError("Please Enter Valid UserName");
+            facultyUserName.requestFocus();
+            return false;
+        } else {
+            facultyUserName.setError(null);
+            facultyUserName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateFacultyPassword() {
+        if (facultyPasswordText.length() != 6) {
+            facultyPassword.setError("Please Enter Valid Password");
+            facultyPassword.requestFocus();
+            return false;
+        } else {
+            facultyPassword.setError(null);
+            facultyPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateStudentUserName() {
+        if (studentUserNameText.length() != 8) {
+            studentUserName.setError("Please Enter Valid UserName");
+            studentUserName.requestFocus();
+            return false;
+        } else {
+            studentUserName.setError(null);
+            studentUserName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateStudentPassword() {
+        if (studentPasswordText.length() != 6) {
+            studentPassword.setError("Please Enter Valid Password");
+            studentPassword.requestFocus();
+            return false;
+        } else {
+            studentPassword.setError(null);
+            studentPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private void initializeFacultyStrings() {
+        facultyUserNameText = facultyUserName.getEditText().getText().toString();
+        facultyPasswordText = facultyPassword.getEditText().getText().toString();
+    }
+
+    private void initializeStudentStrings() {
+        studentUserNameText = studentUserName.getEditText().getText().toString();
+        studentPasswordText = studentPassword.getEditText().getText().toString();
     }
 
     private void initializeFacultyRelViews() {
@@ -96,6 +270,7 @@ public class MainActivity extends AppCompatActivity {
         facultyBtn = findViewById(R.id.loginPage_facultyBtn);
         studentBtn = findViewById(R.id.loginPage_studentBtn);
 
+        relativeLayout = findViewById(R.id.registerPageRelLayout);
         facultyRelLayout = findViewById(R.id.loginPage_facultyRelLayout);
         studentRelLayout = findViewById(R.id.loginPage_studentRelLayout);
 
