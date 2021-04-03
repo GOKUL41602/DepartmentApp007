17d078b3ebbfd1604c2d752a0ea2cda7b2fce463
Gokul41602 <gokul.18it@kct.ac.in>
2021-04-03 13:47:38 +0530

    created SemesterDivisionPage Toolbar

--- app/src/main/java/com/example/loginpage/faculty/FacultyHomePage.java
+++ app/src/main/java/com/example/loginpage/faculty/FacultyHomePage.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.example.loginpage.R;
@@ -30,6 +31,8 @@ public class FacultyHomePage extends AppCompatActivity implements NavigationView
 
     private RecyclerView internRecView;
 
+    private Button firstSemBtn, secondSemBtn, thirdSemBtn, fourthSemBtn, fifthSemBtn, sixthSemBtn, seventhSemBtn, eighthSemBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,17 +44,88 @@ public class FacultyHomePage extends AppCompatActivity implements NavigationView
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         toggle.syncState();
+
+        firstSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+
+        secondSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+
+        thirdSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+        fourthSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+        fifthSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+        sixthSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+        seventhSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+        eighthSemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSemDivisionActivity();
+            }
+        });
+    }
+
+    private void navigateToSemDivisionActivity() {
+        Intent intent = new Intent(FacultyHomePage.this, SemesterDivisionPage.class);
+        startActivity(intent);
     }
 
     private void initializeViews() {
+        // drawerLayout and its contents in facultyHomePage
         drawerLayout = findViewById(R.id.facultyHomePage_drawerLayout);
         toolbar = findViewById(R.id.facultyHomePage_toolbar);
         navigationView = findViewById(R.id.facultyHomePage_nav_view);
 
+        // relative layouts in faculty home page
         empInternRelLayout = findViewById(R.id.facultyHomePage_empRelLayout);
         noEmpInternRelLayout = findViewById(R.id.facultyHomePage_noEmpRelLayout);
         internRecView = findViewById(R.id.facultyHomePage_noEmpRelLayoutRecView);
         semRelLayout = findViewById(R.id.facultyHomePage_booksQuestions_semRelLayout);
+
+        //recyclerView in facultyHomePage-Internship
+        internRecView = findViewById(R.id.facultyHomePage_noEmpRelLayoutRecView);
+
+        //buttons in semRelLayout
+        firstSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_1stSemBtn);
+        secondSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_2ndSemBtn);
+        thirdSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_3rdSemBtn);
+        fourthSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_4thSemBtn);
+        fifthSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_5thSemBtn);
+        sixthSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_6thSemBtn);
+        seventhSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_7thSemBtn);
+        eighthSemBtn = findViewById(R.id.facultyHomePage_booksQuestionsRelLayout_8thSemBtn);
 
     }
 
--- app/src/main/java/com/example/loginpage/faculty/SemesterDivisionPage.java
+++ app/src/main/java/com/example/loginpage/faculty/SemesterDivisionPage.java
@@ -1,16 +1,50 @@
 package com.example.loginpage.faculty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.loginpage.R;
 
 public class SemesterDivisionPage extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
+    private ImageView backBtn;
+
+    private TextView toolbarTitleText;
+
+    private RelativeLayout semDivisionRelLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semester_division_page);
+        initializeViews();
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SemesterDivisionPage.this, FacultyHomePage.class);
+                startActivity(intent);
+                SemesterDivisionPage.this.finish();
+            }
+        });
+    }
+
+    private void initializeViews() {
+        //semesterToolbar views
+        toolbar = findViewById(R.id.semesterDivisionToolBar);
+        backBtn = findViewById(R.id.semesterDivisionBackBtn);
+        toolbarTitleText = findViewById(R.id.semesterDivisionToolBarTitleText);
+
+        //semesterDivision relativeLayout
+        semDivisionRelLayout = findViewById(R.id.semesterDivisionRelLayout);
+
     }
 }
